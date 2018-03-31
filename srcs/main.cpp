daed53f97274ae0f5a2e0668a09b5974b3d5a1ea
Anne-laure VACHER <avacher@e2r3p4.42.fr>
2018-03-31 19:26:56 +0200

    ADD time

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <ncurses.h>
+#include <time.h>
 
 #include "PlayerShip.hpp"
 #include "Missile.hpp"
 #include "MissilePack.hpp"
 #include "Environment.hpp"
-#include <ncurses.h>
 
 int main(void)
 {
     //PlayerShip toto("toto");
     //Missile &pouet = toto.launchMissile();
     Environment env;
+    using namespace std;
+    clock_t begin = clock();
 
     env.handleKey(0);
     while (env.isActive())
     {
+        clock_t end = clock();
+        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        if (elapsed_secs == 10)
+            std::cout << "time : {" << elapsed_secs << "}" << std::endl;
         env.handleKey(getch());
         env.printAll();
     }
+
     return 0;
 }
