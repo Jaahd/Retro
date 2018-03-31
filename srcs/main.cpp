73f6aaa74665745e685c7ebe38547fbea250d21f
Jerome QUEYROU <jqueyrou@e2r4p3.42.fr>
2018-03-31 16:42:44 +0200

    change main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -9,19 +9,14 @@
 int main(void)
 {
     PlayerShip toto("toto");
-    //Missile &pouet = toto.launchMissile();
-    // Environment env;
-    MissilePack mpack;
+    Missile &pouet = toto.launchMissile();
+    Environment env;
 
-    mpack.push(new Missile());
-    mpack.push(new Missile());
-    mpack.push(new Missile());
-
-    // env.handleKey(0);
-    // while (env.isActive())
-    // {
-    //     env.handleKey(getch());
-    //     env.printAll();
-    // }
+    env.handleKey(0);
+    while (env.isActive())
+    {
+        env.handleKey(getch());
+        env.printAll();
+    }
     return 0;
 }
